5d8a2c462fa6dadaea5a7fca8030c17d39b24ec3
admin <admin@promote.cache-dns.local>
2017-06-13 22:24:47 +0800

    Initial commit

---
+++ app/src/main/java/lltan/jikexueyuan/com/lego/adapter/FragmentAdapter.java
@@ -0,0 +1,85 @@
+package lltan.jikexueyuan.com.lego.adapter;
+
+/**
+ * Created by admin on 2017/5/15.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lltan.jikexueyuan.com.lego.fragment.BaseFragment;
+
+
+/**
+ * 功能：主页引导栏的三个Fragment页面设置适配器
+ */
+public class FragmentAdapter extends FragmentPagerAdapter {
+    private List<BaseFragment> fragments;
+
+    public FragmentAdapter(FragmentManager fm, List<BaseFragment> fragments) {
+        super(fm);
+        this.fragments=fragments;
+    }
+
+    public Fragment getItem(int fragment) {
+        return fragments.get(fragment);
+    }
+
+    public int getCount() {
+        return fragments.size();
+    }
+    public Object instantiateItem(final ViewGroup container, final int position) {
+        final View view = LayoutInflater.from(
+                getBaseContext()).inflate(R.layout.item_vp_list, null, false);
+
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(
+                        getBaseContext(), LinearLayoutManager.VERTICAL, false
+                )
+        );
+        recyclerView.setAdapter(new RecycleAdapter());
+
+        container.addView(view);
+        return view;
+    }
+    public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+            final View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_list, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.txt.setText(String.format("Navigation Item #%d", position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView txt;
+
+            public ViewHolder(final View itemView) {
+                super(itemView);
+                txt = (TextView) itemView.findViewById(R.id.txt_vp_item_list);
+            }
+        }
+    }
+}
